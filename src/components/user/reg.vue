82d2016cd6fc702b14189d206f446e30ca5bad66
benson <472756921@QQ.COM>
2017-08-06 10:37:38 +0800

    ''

---
+++ src/components/user/reg.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Input v-model="value11">
+      <span slot="prepend">姓名<span class="redC"> *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></span>
+    </Input>
+    <br/>
+    <Input v-model="value11">
+    <span slot="prepend">身份证号<span class="redC"> *</span></span>
+    </Input>
+    <br/>
+    <Input v-model="value11">
+    <span slot="prepend">联系电话<span class="redC"> *</span></span>
+    </Input>
+    <br/>
+    <Input v-model="value11">
+    <span slot="prepend">联系地址<span class="redC"> *</span></span>
+    </Input>
+    <br/>
+    <div>
+      <span>团队选择：</span>
+      <Button type="primary" size="small">选择团队</Button>
+    </div>
+
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'reg',
+  };
+</script>
+
+<style scoped>
+  .redC{
+    color: red;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Hello from '@/components/Hello';
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';    // 使用 CSS
+import Reg from '@/components/user/reg';
+import Agreement from '@/components/user/agreement';
+
+Vue.use(Router);
+Vue.use(iView);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello,
+    },
+    {
+      path: '/reg',
+      name: 'reg',
+      component: Reg,
+    },
+    {
+      path: '/agreement',
+      name: 'agreement',
+      component: Agreement,
+    },
+  ],
+});
